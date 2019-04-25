b061c37ecd3f4653e71f947e189c77cf477d8e8f
castylho <yohann.castilho94@gmail.com>
2019-04-24 22:36:45 -0300

    fix: remove dead code + update js syntax

--- ClientApp/routes.js
+++ ClientApp/routes.js
@@ -1,13 +1,7 @@
-import React, { Component } from "react";
-import { Switch, Router, Route, browserHistory, hashHistory } from "react-router-dom";
-import { Layout11 } from "./components/Layout";
-import { Layout21 } from "./components/Laylogin";
+import React from "react";
+import { Switch, Router, Route } from "react-router-dom";
 import { Home } from "./components/Home";
-import { FetchData } from "./components/FetchData";
-import { Counter } from "./components/Counter";
 import { Graph } from "./components/graph/Graph";
-import { Mylogin } from "./components/Mylogin";
-import { NavMenu } from "./components/NavMenu";
 import createBrowserHistory from 'history/createBrowserHistory'
 
 const newHistory = createBrowserHistory();
